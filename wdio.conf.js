9b8764aa3aa7f09430ca9bfe72b6dac10ba36d03
Luis Diaz <luis.diaz@wizeline.com>
2019-06-27 19:22:05 -0500

    Added desired capabilities in config + empty test

--- wdio.conf.js
+++ wdio.conf.js
@@ -47,17 +47,27 @@ exports.config = {
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
     capabilities: [{
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
-        maxInstances: 5,
-        //
-        browserName: 'firefox',
-        // If outputDir is provided WebdriverIO can capture driver session logs
-        // it is possible to configure which logTypes to include/exclude.
-        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-        // excludeDriverLogs: ['bugreport', 'server'],
+        appiumVersion: '1.13.0',
+        app: 'com.fitzframes.fitz',
+        deviceName: 'iPhone X',
+        platformVersion: '12.2',
+        platformName: 'iOS',
+        maxInstances: 1,
+        orientation: 'PORTRAIT',
+        automationName: "XCUITest",
     }],
+    // capabilities: [{
+    //     // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+    //     // grid with only 5 firefox instances available you can make sure that not more than
+    //     // 5 instances get started at a time.
+    //     maxInstances: 5,
+    //     //
+    //     browserName: 'firefox',
+    //     // If outputDir is provided WebdriverIO can capture driver session logs
+    //     // it is possible to configure which logTypes to include/exclude.
+    //     // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+    //     // excludeDriverLogs: ['bugreport', 'server'],
+    // }],
     //
     // ===================
     // Test Configurations
